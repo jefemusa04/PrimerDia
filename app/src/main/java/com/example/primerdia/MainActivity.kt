57076923c59c87a46787a046a7a07dc44cfed1f2
jefemusa04e <yisusnando05@gmail.com>
2025-08-26 13:37:37 -0600

    comentario

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerDiaTheme {
+                //Nombre
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
